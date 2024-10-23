ea557fd93cf53740278f82660fa4e83cd2a122bd
J1307-bb <edgarjair2001@hotmail.com>
2024-10-22 19:29:04 -0500

    Add: funcion sumar

--- app.py
+++ app.py
@@ -4,3 +4,9 @@ def greet(name):
 
 if __name__ == "__main__":
     print(greet("World"))
+
+def sumar(a, b):
+    return a + b
+
+if __name__ == "__main__":
+    print(sumar(2, 3))
--- test_app.py
+++ test_app.py
@@ -4,3 +4,8 @@ from app import greet
 def test_greet():
     assert greet("World") == "Hello, World!"
     assert greet("GitHub Actions") == "Hello, GitHub Actions!"
+
+def test_sumar():
+    from app import sumar
+    assert sumar(2, 3) == 5
+    assert sumar(5, 7) == 12
